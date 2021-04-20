ae5780a73e0c0562c1f2c593696a0815697c33cb
Renato Pacheco Vieira <renato.pacheco.r2@gmail.com>
2021-04-19 20:47:38 -0400

    feat(api): add account repository

---
+++ api/src/infra/db/mongodb/account-repository/account.spec.ts
@@ -0,0 +1,34 @@
+import { MongoHelper } from "../helpers/mongo-helpers";
+import { accountData } from "presentation/controllers/signup/mocks";
+import { AccountMongoRepository } from "./account";
+
+describe("Name of the group", () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL);
+  });
+
+  beforeEach(async () => {
+    const collection = MongoHelper.getColllection(
+      AccountMongoRepository.COLLECTION_NAME
+    );
+    await collection.deleteMany({});
+  });
+
+  afterAll(async () => {
+    await MongoHelper.close();
+  });
+
+  const makeSut = (): AccountMongoRepository => new AccountMongoRepository();
+
+  it("should return an account on success", async () => {
+    const sut = makeSut();
+
+    const account = await sut.add(accountData);
+
+    expect(account).toBeTruthy();
+    expect(account.id).toBeTruthy();
+    expect(account.name).toEqual(accountData.name);
+    expect(account.email).toEqual(accountData.email);
+    expect(account.password).toEqual(accountData.password);
+  });
+});
---
+++ api/src/infra/db/mongodb/account-repository/account.ts
@@ -0,0 +1,16 @@
+import { AddAccountRepository } from "data/protocols/add-account-repository";
+import { AccountModel } from "domain/models/accounts";
+import { AccountData } from "domain/usecases/add-account";
+import { MongoHelper } from "../helpers/mongo-helpers";
+
+export class AccountMongoRepository implements AddAccountRepository {
+  static COLLECTION_NAME = "accounts";
+
+  async add(accountData: AccountData): Promise<AccountModel> {
+    const collection = MongoHelper.getColllection(
+      AccountMongoRepository.COLLECTION_NAME
+    );
+    const result = await collection.insertOne(accountData);
+    return MongoHelper.map(result.ops[0]);
+  }
+}
