9e5ed54cb987228d9b8b289d9d3cacab51904a62
Renato Pacheco Vieira <renato.pacheco.r2@gmail.com>
2021-04-19 20:17:00 -0400

    feat(api): add MongoHelper

---
+++ api/src/infra/db/mongodb/helpers/mongo-helpers.ts
@@ -0,0 +1,28 @@
+import { Collection, MongoClient } from "mongodb";
+
+export const MongoHelper = {
+  client: null as MongoClient,
+
+  async connect(url: string): Promise<void> {
+    this.client = await MongoClient.connect(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+  },
+
+  async close() {
+    this.client.close();
+  },
+
+  getColllection(name: string): Collection {
+    return this.client.db().collection(name);
+  },
+
+  map(collection: any): any {
+    const { _id: id, ...rest } = collection;
+    return {
+      ...rest,
+      id,
+    };
+  },
+};
